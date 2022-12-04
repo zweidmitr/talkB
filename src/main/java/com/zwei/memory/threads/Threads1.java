e25fac0437f503977b3100aa50f7b3e57c9fa5c4
zweidmitr <zweidmitr@gmail.com>
2022-12-04 23:41:07 +0300

    memory:sleep, yield,join

--- src/main/java/com/zwei/memory/threads/Threads1.java
+++ src/main/java/com/zwei/memory/threads/Threads1.java
@@ -1,11 +1,15 @@
 package com.zwei.memory.threads;
 
+// основная идея многопоточности - контроль потоков
 public class Threads1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("hello world - " + Thread.currentThread().getName());
 
         MyThread myThread = new MyThread(); // new
         myThread.start(); // ready
+
+//        Thread.sleep(1000); // main засыпает на время
+        myThread.join(); // main ждет пока myThread не закончит свою работу
 
         MyThreadRunnable r1 = new MyThreadRunnable();
         Thread t2 = new Thread(r1);
@@ -20,6 +24,7 @@ class MyThread extends Thread {
     public void run() { // Running
         for (int i = 0; i < 10; i++)
             System.out.println(i + " " + Thread.currentThread().getName());
+//        Thread.yield(); // уступает поток другим, работает на 90% ... вопросы к ОС
     }
 }
 
